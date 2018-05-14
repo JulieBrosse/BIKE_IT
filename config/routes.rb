5f883f191cc345c7b4b4939ea48c27800e75cc1e
Mathieu Duprez <duprez.math@gmail.com>
2018-05-14 19:02:24 +0200

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :bikes, only: [:index, :show, :create, :update] do
+    resources :bikes_rents, only: [:create]
+  end
+  resources :bikes_rents, only: [:index]
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
