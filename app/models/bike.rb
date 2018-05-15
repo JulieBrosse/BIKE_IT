a68365cffdcdfdcf462e5818a2a0931abce5d32f
Martin Solignac <martin.solignac@gmail.com>
2018-05-15 16:25:39 +0200

    show view bike + home form

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,5 +1,5 @@
 class Bike < ApplicationRecord
-  validates :title, presence: true, allow_blank: false
+  validates :title, presence: true
   validates :availability, presence: true
   validates :price, presence: true, allow_blank: false
   belongs_to :tenant, class_name: "User", foreign_key: "tenant_id"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180514152855) do
+ActiveRecord::Schema.define(version: 20180515131747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,10 +29,12 @@ ActiveRecord::Schema.define(version: 20180514152855) do
     t.text "description"
     t.string "title"
     t.integer "price"
-    t.boolean "availability"
+    t.boolean "availability", default: true
     t.bigint "tenant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
+    t.string "picture"
     t.index ["tenant_id"], name: "index_bikes_on_tenant_id"
   end
 
