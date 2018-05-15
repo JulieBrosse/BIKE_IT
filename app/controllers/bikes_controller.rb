076c5f58ceb527edb545c9172e1f6188f15e767f
Mathieu Duprez <duprez.math@gmail.com>
2018-05-15 14:23:27 +0200

    save the branch to github

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,14 +1,41 @@
 class BikesController < ApplicationController
 
   def index
+    if params[:search].present?
+      address = params[:search][:address]
+      @bikes = Bike.where(address: address)
+    else
+      @bikes = Bike.all
+    end
   end
 
-  def new
+
+  def show
+    @bike = Bike.find(params[:id])
   end
 
   def create
+    @bike = Bike.new(bike_params)
+      if @bike.save
+        redirect_to bikes_rents_path
+      end
   end
 
+  def update
+    @bike = Bike.find(params[:id])
+    @bike.update(bike_params)
+    redirect_to bike_path(@bike)
+  end
 
+  private
+
+  def bike_params
+    params.require(:bike).permit(:title, :price, :availability, :description)
+
+  end
 
 end
+
+
+
+
