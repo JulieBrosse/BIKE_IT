7b5af08abcfaab49e122f62cca860cd908cd9df0
Martin Solignac <martin.solignac@gmail.com>
2018-05-16 12:37:49 +0200

    solve issues with controllers

--- app/controllers/bike_rents_controller.rb
+++ app/controllers/bike_rents_controller.rb
@@ -1,22 +1,22 @@
-class BikeRentsController < ApplicationRecord
-
+class BikeRentsController < ApplicationController
  def index
-  @bikes_rents = Bike.where(current_user_id == renter_id)
+  @bike_rents = current_user.bike_rents
+  @bikes_rented = BikeRent.includes(:bike).where(bike: current_user.bikes)
  end
 
- def create
-  @bike_rent = BikeRent.find(params[:id])
-  @bike_rent.save
- end
+ # def create
+ #  @bike_rent = BikeRent.find(params[:id])
+ #  @bike_rent.save
+ # end
 
- def update
-  @bike_rent = BikeRent.find(params[:id])
-  @bike_rent.update(bike_rent_params)
- end
+ # def update
+ #  @bike_rent = BikeRent.find(params[:id])
+ #  @bike_rent.update(bike_rent_params)
+ # end
 
- private
+ # private
 
- def bikes_rent_params
-  params.require(:bike).permit(:date, :renter_id, :bike_id)
- end
+ # def bikes_rent_params
+ #  params.require(:bike).permit(:date, :renter_id, :bike_id)
+ # end
 end
