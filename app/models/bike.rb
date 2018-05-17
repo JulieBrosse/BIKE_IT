4fe4a724194227a14d6c705369f0a0961590a018
Julie Brosse <juliebrosse31@gmail.com>
2018-05-17 12:08:19 +0200

    create a bike - first steps

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -3,4 +3,5 @@ class Bike < ApplicationRecord
   validates :availability, presence: true
   validates :price, presence: true, allow_blank: false
   belongs_to :tenant, class_name: "User", foreign_key: "tenant_id"
+  mount_uploader :picture, PhotoUploader
 end
