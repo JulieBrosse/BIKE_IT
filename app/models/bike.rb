9f22b42a03c0d114b1684df66573d0bd5fafb8d6
Julie Brosse <juliebrosse31@gmail.com>
2018-05-18 15:06:19 +0200

    form_with_address

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -2,6 +2,7 @@ class Bike < ApplicationRecord
   validates :title, presence: true
   validates :availability, presence: true
   validates :price, presence: true, allow_blank: false
+  validates :address, presence: true, allow_blank: false
   belongs_to :tenant, class_name: "User", foreign_key: "tenant_id"
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
